20e31d6751c665d747033f1e782975f7726918c1
krishna <sohala.krishna49@gmail.com>
2020-01-04 12:27:42 +0530

    my first commit in this prokecnfj

---
+++ krishna123.com/src/main/java/krishnasohala/krishna123/com/App.java
@@ -0,0 +1,13 @@
+package krishnasohala.krishna123.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
